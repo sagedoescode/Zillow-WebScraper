2f77b9d118c2e9ddbab2703cf6567b10e6000a8f
ranielcsar <whyfck@gmail.com>
2024-03-20 17:19:28 -0300

    fix: address types and formats

--- extractor.js
+++ extractor.js
@@ -32,82 +32,119 @@ async function waitForElement(selector, timeout = 3000) {
     });
 }
 
-async function extractData() {
-    try {
-        await waitForElement('div[data-testid="data-column"]');
-        const dataView = await waitForElement('div[data-testid="data-view"]');
+function findOverview(elements) {
+    let target = null;
 
-        const h2Elements = dataView.querySelectorAll("h2");
-
-        let targetH2 = null;
-        for (const h2 of h2Elements) {
-            if (h2.innerText.toLowerCase().includes("overview")) {
-                targetH2 = h2;
-                break;
-            }
+    for (const el of elements) {
+        if (el.innerText.toLowerCase().includes("overview")) {
+            target = el;
+            break;
         }
-
-        if (targetH2) {
-            const h2OffsetTop = targetH2.offsetTop;
-            dataView.scrollTop = h2OffsetTop;
-        }
-
-        const h4Elements = dataView.querySelectorAll("h4");
-
-        let targetH4 = null;
-        for (const h4 of h4Elements) {
-            if (h4.innerText.toLowerCase().includes("overview")) {
-                targetH4 = h4;
-                break;
-            }
-        }
-
-        if (targetH4) {
-            const h4OffsetTop = targetH4.offsetTop;
-            dataView.scrollTop = h4OffsetTop;
-        }
-
-        await waitForElement('div[class="ds-listing-agent-header"]');
-        await waitForElement('div[class="ds-listing-agent-container"]');
-        await waitForElement('ul[class="ds-listing-agent-info"]');
-
-        let tabData = {};
-
-        let buildingAddress = document.querySelector(
-            'h2[data-test-id="bdp-building-address"]'
-        )?.innerHTML;
-
-        if (!buildingAddress) {
-            buildingAddress = document.querySelector(
-                'h1[class^="Text-c11n"]'
-            )?.innerHTML;
-        }
-
-        const [address, city, stateZip] = buildingAddress.split(",");
-        const [state, zipcode] = stateZip
-            .split(" ")
-            .filter((string) => string !== "");
-
-        const listingAgent = document.querySelector(
-            'div[class="ds-listing-agent-header"]'
-        )?.innerHTML;
-
-        const agentName = document.querySelector(
-            'span[class="ds-listing-agent-business-name"]'
-        )?.innerHTML;
-
-        tabData["listedBy"] = decodeHtmlEntities(listingAgent);
-        tabData["name"] = decodeHtmlEntities(agentName);
-        tabData["address"] = decodeHtmlEntities(address.trim());
-        tabData["city"] = decodeHtmlEntities(city.trim());
-        tabData["state"] = decodeHtmlEntities(state.trim());
-        tabData["zipcode"] = decodeHtmlEntities(zipcode.trim());
-
-        return tabData;
-    } catch (error) {
-        console.error(error);
-        return {};
     }
+
+    if (target) {
+        return target.offsetTop + 100;
+    }
+
+    return target;
+}
+
+function getAddress() {
+    let buildingAddress = document.querySelector(
+        'h2[data-test-id="bdp-building-address"]'
+    );
+
+    if (!buildingAddress) {
+        buildingAddress = document.querySelector('h1[class^="Text-c11n"]');
+    }
+
+    if (!buildingAddress) {
+        buildingAddress = document.querySelector('h1[class^="Text-c22n"]');
+    }
+
+    const addressBtn = buildingAddress.querySelector("button");
+
+    if (addressBtn) {
+        const buttonText = addressBtn.innerHTML;
+        addressBtn.remove();
+
+        const remainingText = buildingAddress.innerHTML;
+        const concatenatedText = `${buttonText} ${remainingText}`;
+
+        buildingAddress = concatenatedText;
+    }
+
+    let addressMatch;
+    const addressRegex = /([^,]+),\s*([^,]+),\s*([A-Za-z]+)\s*(\d{5})/;
+
+    if (typeof buildingAddress === "string") {
+        buildingAddress = buildingAddress.trim();
+        addressMatch = buildingAddress.match(addressRegex);
+    } else {
+        addressMatch = buildingAddress.innerHTML.match(addressRegex);
+    }
+
+    return addressMatch;
+}
+
+async function extractData() {
+    await waitForElement('div[data-testid="data-column"]');
+    const dataView = await waitForElement('div[data-testid="data-view"]');
+
+    const h4Elements = await dataView.querySelectorAll("h4");
+    const targetH4 = findOverview(h4Elements);
+    if (targetH4) {
+        dataView.scrollTop = targetH4.offsetTop;
+    }
+
+    const h2Elements = await dataView.querySelectorAll("h2");
+    const targetH2 = await findOverview(h2Elements);
+    if (targetH2) {
+        dataView.scrollTop = targetH2.offsetTop;
+    }
+
+    await waitForElement('div[class="ds-listing-agent-header"]');
+    await waitForElement('div[class="ds-listing-agent-container"]');
+    await waitForElement('ul[class="ds-listing-agent-info"]');
+
+    let tabData = {};
+
+    const address = getAddress();
+
+    const listingAgent = document.querySelector(
+        'div[class="ds-listing-agent-header"]'
+    )?.innerHTML;
+
+    const agentName = document.querySelector(
+        'span[class="ds-listing-agent-business-name"]'
+    )?.innerHTML;
+
+    tabData["listedBy"] = decodeHtmlEntities(listingAgent);
+    tabData["name"] = decodeHtmlEntities(agentName);
+
+    tabData["address"] = decodeHtmlEntities(address[1].trim());
+    const unit = decodeHtmlEntities(address[0].trim());
+
+    if (address[1].includes("#")) {
+        tabData["address"] = `${address.input.split(",")[0]}`;
+    }
+
+    if (unit.includes("#")) {
+        tabData["address"] = `${tabData["address"]} ${address.input
+            .split(",")[1]
+            .trim()}`;
+    }
+
+    tabData["city"] = decodeHtmlEntities(
+        address[2]
+            .trim()
+            .replace(/<\/?[^>]+(>|$)/g, "")
+            .trim()
+    );
+    tabData["state"] = decodeHtmlEntities(address[3].trim());
+    tabData["zipcode"] = decodeHtmlEntities(address[4].trim());
+
+    return tabData;
 }
 
 function decodeHtmlEntities(html) {
@@ -115,7 +152,7 @@ function decodeHtmlEntities(html) {
 
     var txt = document.createElement("textarea");
     txt.innerHTML = html;
-    return txt.value;
+    return txt.value.trim();
 }
 
 extractData();
