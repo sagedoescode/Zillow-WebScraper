763274f6e4a20304a783e8006756210e24b7c1a7
ranielcsar <whyfck@gmail.com>
2024-03-21 07:53:00 -0300

    fix: scroll until find the "Listed by"

--- extractor.js
+++ extractor.js
@@ -6,47 +6,36 @@ async function waitForElement(selector, timeout = 3000) {
             observer.disconnect();
             reject(
                 new Error(
-                    `Timeout: Elemento "${selector}" não encontrado após ${timeout}ms`
+                    `Timeout: Element "${selector}" not found after ${timeout}ms`
                 )
             );
         }, timeout);
 
-        if (document.querySelector(selector)) {
-            clearTimeout(timer);
-            return resolve(document.querySelector(selector));
-        }
+        const dataView = document.querySelector('div[data-testid="data-view"]');
 
         const observer = new MutationObserver((mutations) => {
-            if (document.querySelector(selector)) {
+            const element = document.querySelector(selector);
+            if (element) {
                 clearTimeout(timer);
                 observer.disconnect();
-                resolve(document.querySelector(selector));
+                resolve(element);
+            } else {
+                dataView.scrollTop += 100;
             }
         });
 
-        // If you get "parameter 1 is not of type 'Node'" error, see https://stackoverflow.com/a/77855838/492336
         observer.observe(document.body, {
             childList: true,
             subtree: true,
         });
-    });
-}
 
-function findOverview(elements) {
-    let target = null;
-
-    for (const el of elements) {
-        if (el.innerText.toLowerCase().includes("overview")) {
-            target = el;
-            break;
+        const element = document.querySelector(selector);
+        if (element) {
+            clearTimeout(timer);
+            observer.disconnect();
+            resolve(element);
         }
-    }
-
-    if (target) {
-        return target.offsetTop + 100;
-    }
-
-    return target;
+    });
 }
 
 function getAddress() {
@@ -89,38 +78,10 @@ function getAddress() {
 
 async function extractData() {
     await waitForElement('div[data-testid="data-column"]');
-    const dataView = await waitForElement('div[data-testid="data-view"]');
-
-    const h4Elements = await dataView.querySelectorAll("h4");
-    const targetH4 = findOverview(h4Elements);
-    if (targetH4) {
-        dataView.scrollTop = targetH4.offsetTop;
-    }
-
-    const h2Elements = await dataView.querySelectorAll("h2");
-    const targetH2 = await findOverview(h2Elements);
-    if (targetH2) {
-        dataView.scrollTop = targetH2.offsetTop;
-    }
-
-    await waitForElement('div[class="ds-listing-agent-header"]');
-    await waitForElement('div[class="ds-listing-agent-container"]');
-    await waitForElement('ul[class="ds-listing-agent-info"]');
+    await waitForElement('div[data-testid="data-view"]');
 
     let tabData = {};
-
     const address = getAddress();
-
-    const listingAgent = document.querySelector(
-        'div[class="ds-listing-agent-header"]'
-    )?.innerHTML;
-
-    const agentName = document.querySelector(
-        'span[class="ds-listing-agent-business-name"]'
-    )?.innerHTML;
-
-    tabData["listedBy"] = decodeHtmlEntities(listingAgent);
-    tabData["name"] = decodeHtmlEntities(agentName);
 
     tabData["address"] = decodeHtmlEntities(address[1].trim());
     const unit = decodeHtmlEntities(address[0].trim());
@@ -143,6 +104,27 @@ async function extractData() {
     );
     tabData["state"] = decodeHtmlEntities(address[3].trim());
     tabData["zipcode"] = decodeHtmlEntities(address[4].trim());
+
+    try {
+        await waitForElement('div[class="ds-listing-agent-header"]');
+        await waitForElement('div[class="ds-listing-agent-container"]');
+        await waitForElement('ul[class="ds-listing-agent-info"]');
+
+        const listingAgent = document.querySelector(
+            'div[class="ds-listing-agent-header"]'
+        )?.innerHTML;
+
+        const agentName = document.querySelector(
+            'span[class="ds-listing-agent-business-name"]'
+        )?.innerHTML;
+
+        tabData["listedBy"] = decodeHtmlEntities(listingAgent);
+        tabData["name"] = decodeHtmlEntities(agentName);
+    } catch (error) {
+        console.error("error on finding agent infos");
+        tabData["listedBy"] = "";
+        tabData["name"] = "";
+    }
 
     return tabData;
 }
